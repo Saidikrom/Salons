2da3876d00c6a017876b6517403e8ae39d849124
Saidikrom <yusupovsaidikrom9@gmail.com>
2024-11-04 01:04:18 +0500

    key secured

--- lib/core/constants/urls_const.dart
+++ lib/core/constants/urls_const.dart
@@ -2,6 +2,6 @@ class UrlsConst {
   static String salons_url = "https://api.demand24.org/api/v1/rest/shops/paginate?";
   static String autocomplete_url = "https://maps.googleapis.com/maps/api/place/autocomplete/json";
   static String placeDetails_url = "https://maps.googleapis.com/maps/api/place/details/json";
-  static String apiKey = "AIzaSyBbfnvObzwcs6OLXOmSaG9CtIhwjmBwLTQ";
+
 
 }
